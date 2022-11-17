69d52aaa20357a08cced7be59672f0f012fedf8d
ADMIN\Dell <vannvph28656@fpt.edu.vn>
2022-11-17 20:25:46 +0700

    update db vs project

---
+++ dbqlHV.sql
@@ -0,0 +1,218 @@
+﻿--create database QLHV
+--go
+
+use QLHV ;
+
+go
+
+if OBJECT_ID('ChucVu') is not null
+drop table ChucVu
+go
+create table ChucVu
+(
+    idChucVu int PRIMARY KEY not null,
+    tenChucVu NVARCHAR(50) not null,
+	ngayTao date,
+	ngaySua date
+)
+if OBJECT_ID('NhanVien') is not null
+drop table NhanVien
+go
+create table NhanVien
+(
+    id UNIQUEIDENTIFIER DEFAULT newID() PRIMARY KEY,
+    ho NVARCHAR(10) ,
+    tenDem NVARCHAR(10) ,
+    ten NVARCHAR(10),
+    email NVARCHAR(50)not null,
+    diaChi NVARCHAR(50),
+	sdt char(11),
+	ngaySinh date,
+    trangThai NVARCHAR(50),
+    matKhau varchar(50) not null,
+    idChucVu int,
+	ngayTao date,
+    ngaySua date,
+    FOREIGN KEY (idChucVu) REFERENCES ChucVu
+)
+
+if OBJECT_ID('HocKy') is not null
+drop table HocKy
+go
+create table HocKy
+(  
+    idHocKy UNIQUEIDENTIFIER DEFAULT newID() PRIMARY KEY,
+    hocKy int not null,
+    moTa NVARCHAR(50),
+    thoiLuong nvarchar(50) not null,
+    ngayTao date,
+    ngaySua date
+)
+
+if OBJECT_ID('HocVien') is not null
+drop table HocVien
+go
+create table HocVien
+(
+    id UNIQUEIDENTIFIER DEFAULT newID() PRIMARY KEY,
+    ho NVARCHAR(10) ,
+    tenDem NVARCHAR(10) ,
+    ten NVARCHAR(10),
+    email NVARCHAR(50)not null,
+    diaChi NVARCHAR(50),
+	sdt char(11),
+	ngaySinh date,
+    trangThai NVARCHAR(50),
+    matKhau varchar(50) not null,
+	ngayTao date,
+    ngaySua date
+)
+
+if OBJECT_ID('CaHoc') is not null
+drop table CaHoc
+go
+create table CaHoc
+(   
+    id UNIQUEIDENTIFIER DEFAULT newID() PRIMARY KEY,
+    maCa varchar(10) not null,
+    thoiGian varchar(50) not null,
+    ngayTao date,
+    ngaySua date
+)
+
+
+
+if OBJECT_ID('DangKyHocLai') is not null
+drop table DangKyHocLai
+go
+create table DangKyHocLai
+(	
+	idDangKyHocLai UNIQUEIDENTIFIER default newID(),
+    idHocVien UNIQUEIDENTIFIER ,
+    idHocKy UNIQUEIDENTIFIER ,
+    ngayDangKy date ,
+	hocPhi decimal ,
+	ngayTao date,
+    ngaySua date,
+	PRIMARY KEY (idDangKyHocLai),
+    FOREIGN KEY (idHocKy) REFERENCES HocKy,
+    FOREIGN KEY (idHocVien) REFERENCES HocVien
+)
+if OBJECT_ID('Vi') is not null
+drop table Vi
+go
+create table Vi
+(
+    idVi UNIQUEIDENTIFIER DEFAULT newID() PRIMARY KEY,
+    idHocVien UNIQUEIDENTIFIER not null,
+    soDuVi DECIMAL ,
+    ngayTao date,
+    ngaySua date,
+    trangThai NVARCHAR(50),
+    FOREIGN KEY (idHocVien) REFERENCES HocVien
+)
+if OBJECT_ID('MonHoc') is not null
+drop table MonHoc
+go
+create table MonHoc
+(
+    id UNIQUEIDENTIFIER DEFAULT newID() PRIMARY KEY not null,
+    idHocKy UNIQUEIDENTIFIER ,
+	tenMon nvarchar(50),
+	hocPhi decimal,
+	thoiLuong int check(thoiLuong>=1 and thoiLuong<=12),
+	ngayTao date,
+    ngaySua date,
+    FOREIGN KEY(idHocKy) REFERENCES HocKy,
+)
+if OBJECT_ID('Lop') is not null
+drop table Lop
+go
+create table Lop
+(   
+    id UNIQUEIDENTIFIER DEFAULT newID() PRIMARY KEY,
+	idGiangVien UNIQUEIDENTIFIER ,
+    maLop NVARCHAR(50),
+    tenLop NVARCHAR(50),
+    slHocVien int ,
+    slToiDa int ,
+    idCa UNIQUEIDENTIFIER not null,
+	idMonHoc UNIQUEIDENTIFIER not null,
+    FOREIGN KEY (idGiangVien) REFERENCES NhanVien,
+    FOREIGN KEY (idCa) REFERENCES CaHoc,
+	FOREIGN KEY (idMonHoc) REFERENCES MonHoc
+)
+if OBJECT_ID('LopHocVien') is not null
+drop table LopHocVien
+go
+create table LopHocVien
+(
+	idLopHocVien UNIQUEIDENTIFIER default newid(),
+    idLop UNIQUEIDENTIFIER not null,
+    idHocVien UNIQUEIDENTIFIER not null,
+	primary key(idLopHocVien),
+    FOREIGN KEY (idLop) REFERENCES Lop,
+    FOREIGN KEY (idHocVien) REFERENCES HocVien
+)
+if OBJECT_ID('DongHocPhi') is not null
+drop table DongHocPhi
+go
+create table DongHocPhi
+(
+    idDongHP UNIQUEIDENTIFIER DEFAULT newID() PRIMARY KEY not null,
+    idHocVien UNIQUEIDENTIFIER ,
+	idMonHoc UNIQUEIDENTIFIER,
+    ngayDong DATE ,
+    tienDong DECIMAL,
+	trangThai NVARCHAR(50),
+    ngayTao date,
+    ngaySua date,
+    FOREIGN KEY(idHocVien) REFERENCES HocVien,
+	FOREIGN KEY(idMonHoc) REFERENCES MonHoc	
+)
+
+if OBJECT_ID('KetQuaHT') is not null
+drop table KetQuaHT
+go
+create table KetQuaHT
+(
+    id UNIQUEIDENTIFIER DEFAULT newID() PRIMARY KEY,
+    idHocVien UNIQUEIDENTIFIER not null,
+	idMonHoc UNIQUEIDENTIFIER not null,
+	diemGiuaKy float,
+	diemTongKet float,
+	danhGia nvarchar(50),
+	ngayTao date,
+    ngaySua date,
+    FOREIGN KEY(idHocVien) REFERENCES HocVien,
+	FOREIGN KEY(idMonHoc) REFERENCES MonHoc,
+)
+create table LichHoc
+(
+    id UNIQUEIDENTIFIER DEFAULT newID() PRIMARY KEY,
+    idLop UNIQUEIDENTIFIER not null,
+	ngayHoc Date,
+	ngayTao date,
+    ngaySua date,
+    FOREIGN KEY(idLop) REFERENCES Lop
+
+)
+
+delete from NhanVien
+select*from NhanVien
+select*from ChucVu
+
+insert into ChucVu(idChucVu,tenChucVu,ngayTao,ngaySua)
+values
+('0',N'Giảng Viên','2022/11/15','2022/11/15'),
+('1','Admin','2022/11/15','2022/11/15')
+
+insert into NhanVien(ho,tenDem,ten,email,diaChi,sdt,trangThai,matKhau,idChucVu,ngayTao,ngaySua)
+values 
+('Vu','Van','Nguyen','nguyenvv4@fpt.edu.vn','HN','0999999999','dang day','1234','0','2022/11/15','2022/11/15'),
+('Ha','Quoc','Khach','khanhhq@fpt.edu.vn','HN','0999999999','dang day','1234','0','2022/11/15','2022/11/15'),
+('Chu','Van','Hieu','hieucv@fpt.edu.vn','HN','0999999999','dang day','1234','0','2022/11/15','2022/11/15'),
+('Tran','Duc','Do','dotd@fpt.edu.vn','HN','0999999999','dang day','1234','0','2022/11/15','2022/11/15'),
+('Mai','Ngoc','Minh','minhmn@fpt.edu.vn','HN','0999999999','dang day','1234','0','2022/11/15','2022/11/15'),
+('Van','Van','Van','vannvph28656@fpt.edu.vn','HN','0999999999','admin','1234','1','2022/11/15','2022/11/15')
+
---
+++ duan1_JDBC/src/view/formLogin.java
@@ -0,0 +1,199 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+package view;
+
+/**
+ *
+ * @author Dell
+ */
+public class formLogin extends javax.swing.JFrame {
+
+    /**
+     * Creates new form login
+     */
+    public formLogin() {
+        initComponents();
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jButton1 = new javax.swing.JButton();
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        btnloginTrangChu = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        txtTaiKhoan = new javax.swing.JTextField();
+        txtMatKhau = new javax.swing.JTextField();
+        btnDangXuat = new javax.swing.JButton();
+        btnDangNhap = new javax.swing.JButton();
+
+        jButton1.setText("jButton1");
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+        jLabel1.setText("Đăng Nhập");
+
+        btnloginTrangChu.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnloginTrangChuActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 164, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnloginTrangChu))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1)
+                    .addComponent(btnloginTrangChu, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(22, Short.MAX_VALUE))
+        );
+
+        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel2.setText("Tài Khoản");
+
+        jLabel3.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel3.setText("Mật Khẩu");
+
+        btnDangXuat.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        btnDangXuat.setText("Thoát");
+        btnDangXuat.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnDangXuatActionPerformed(evt);
+            }
+        });
+
+        btnDangNhap.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        btnDangNhap.setText("Đăng Nhập");
+        btnDangNhap.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnDangNhapActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(26, 26, 26)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(btnDangXuat)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel2))
+                        .addGap(29, 29, 29)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(txtMatKhau, javax.swing.GroupLayout.PREFERRED_SIZE, 274, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txtTaiKhoan, javax.swing.GroupLayout.PREFERRED_SIZE, 274, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addGap(0, 38, Short.MAX_VALUE))
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(101, 101, 101)
+                    .addComponent(btnDangNhap)
+                    .addContainerGap(190, Short.MAX_VALUE)))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(53, 53, 53)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(txtTaiKhoan, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(66, 66, 66)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(txtMatKhau, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 87, Short.MAX_VALUE)
+                .addComponent(btnDangXuat)
+                .addGap(47, 47, 47))
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                    .addContainerGap(356, Short.MAX_VALUE)
+                    .addComponent(btnDangNhap)
+                    .addGap(47, 47, 47)))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnloginTrangChuActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnloginTrangChuActionPerformed
+       
+    }//GEN-LAST:event_btnloginTrangChuActionPerformed
+
+    private void btnDangXuatActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDangXuatActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_btnDangXuatActionPerformed
+
+    private void btnDangNhapActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDangNhapActionPerformed
+       
+       
+    }//GEN-LAST:event_btnDangNhapActionPerformed
+
+    
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(formLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(formLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(formLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(formLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new formLogin().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnDangNhap;
+    private javax.swing.JButton btnDangXuat;
+    private javax.swing.JButton btnloginTrangChu;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JTextField txtMatKhau;
+    private javax.swing.JTextField txtTaiKhoan;
+    // End of variables declaration//GEN-END:variables
+}
